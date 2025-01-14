1f0243094b7accb7cce1b8886489bb88365cf34d
Carlos Caño <canogomezcarlos@gmail.com>
2025-01-14 11:47:19 +0100

    Creando proyecto de pruebas

---
+++ ConcurrenciaEstrictaPruebas/ConcurrenciaEstrictaPruebasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrenciaEstrictaPruebasApp.swift
+//  ConcurrenciaEstrictaPruebas
+//
+//  Created by Carlos C on 14/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrenciaEstrictaPruebasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
